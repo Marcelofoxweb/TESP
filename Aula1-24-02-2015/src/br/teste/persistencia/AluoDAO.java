f5af262e9d56b517eb142ea5cdaa1e7d2ab25731
Marcelo Silva <marcelo.foxweb@gmail.com>
2015-03-09 22:30:45 -0300

    Aula dia 09/03/2015

---
+++ Aula1-24-02-2015/src/br/teste/persistencia/AluoDAO.java
@@ -0,0 +1,39 @@
+package br.teste.persistencia;
+
+import java.util.List;
+
+import br.teste.entidade.Aluno;
+
+public class AluoDAO implements DAO<Aluno, Long> {
+
+	@Override
+	public Aluno find(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Aluno t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Aluno t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Aluno t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Aluno> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
